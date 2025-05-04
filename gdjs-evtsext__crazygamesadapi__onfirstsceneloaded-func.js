3a09afaf1ad0a0401575be23137ee01196e291db
DonAnton20 <antony.janson20@gmail.com>
2025-05-04 15:06:16 +0200

    Add files via upload

---
+++ gdjs-evtsext__crazygamesadapi__onfirstsceneloaded-func.js
@@ -0,0 +1,105 @@
+
+if (typeof gdjs.evtsExt__CrazyGamesAdApi__onFirstSceneLoaded !== "undefined") {
+  gdjs.evtsExt__CrazyGamesAdApi__onFirstSceneLoaded.registeredGdjsCallbacks.forEach(callback =>
+    gdjs._unregisterCallback(callback)
+  );
+}
+
+gdjs.evtsExt__CrazyGamesAdApi__onFirstSceneLoaded = {};
+
+
+gdjs.evtsExt__CrazyGamesAdApi__onFirstSceneLoaded.userFunc0x913168 = function GDJSInlineCode(runtimeScene, eventsFunctionContext) {
+"use strict";
+gdjs._crazyGamesExtension = {
+    isSdkReady: false,
+    isPlayingVideoAd: false,
+    hasVideoAdJustFinishedPlaying: false,
+    adHadAnError: false,
+    lastErrorDescription: "",
+    linkAccountResponse: "",
+    isAdBlockerDetected: false,
+    links: new Map(),
+    userChanged: false,
+    userId: null,
+    userSignedIn: false,
+    userToken: null,
+    xsollaToken: null,
+};
+};
+gdjs.evtsExt__CrazyGamesAdApi__onFirstSceneLoaded.eventsList0 = function(runtimeScene, eventsFunctionContext) {
+
+{
+
+
+gdjs.evtsExt__CrazyGamesAdApi__onFirstSceneLoaded.userFunc0x913168(runtimeScene, typeof eventsFunctionContext !== 'undefined' ? eventsFunctionContext : undefined);
+
+}
+
+
+};
+
+gdjs.evtsExt__CrazyGamesAdApi__onFirstSceneLoaded.func = function(runtimeScene, parentEventsFunctionContext) {
+var eventsFunctionContext = {
+  _objectsMap: {
+},
+  _objectArraysMap: {
+},
+  _behaviorNamesMap: {
+},
+  globalVariablesForExtension: runtimeScene.getGame().getVariablesForExtension("CrazyGamesAdApi"),
+  sceneVariablesForExtension: runtimeScene.getScene().getVariablesForExtension("CrazyGamesAdApi"),
+  localVariables: [],
+  getObjects: function(objectName) {
+    return eventsFunctionContext._objectArraysMap[objectName] || [];
+  },
+  getObjectsLists: function(objectName) {
+    return eventsFunctionContext._objectsMap[objectName] || null;
+  },
+  getBehaviorName: function(behaviorName) {
+    return eventsFunctionContext._behaviorNamesMap[behaviorName] || behaviorName;
+  },
+  createObject: function(objectName) {
+    const objectsList = eventsFunctionContext._objectsMap[objectName];
+    if (objectsList) {
+      const object = parentEventsFunctionContext ?
+        parentEventsFunctionContext.createObject(objectsList.firstKey()) :
+        runtimeScene.createObject(objectsList.firstKey());
+      if (object) {
+        objectsList.get(objectsList.firstKey()).push(object);
+        eventsFunctionContext._objectArraysMap[objectName].push(object);
+      }
+      return object;    }
+    return null;
+  },
+  getInstancesCountOnScene: function(objectName) {
+    const objectsList = eventsFunctionContext._objectsMap[objectName];
+    let count = 0;
+    if (objectsList) {
+      for(const objectName in objectsList.items)
+        count += parentEventsFunctionContext ?
+parentEventsFunctionContext.getInstancesCountOnScene(objectName) :
+        runtimeScene.getInstancesCountOnScene(objectName);
+    }
+    return count;
+  },
+  getLayer: function(layerName) {
+    return runtimeScene.getLayer(layerName);
+  },
+  getArgument: function(argName) {
+    return "";
+  },
+  getOnceTriggers: function() { return runtimeScene.getOnceTriggers(); }
+};
+
+
+gdjs.evtsExt__CrazyGamesAdApi__onFirstSceneLoaded.eventsList0(runtimeScene, eventsFunctionContext);
+
+
+return;
+}
+
+gdjs.evtsExt__CrazyGamesAdApi__onFirstSceneLoaded.registeredGdjsCallbacks = [];
+gdjs.evtsExt__CrazyGamesAdApi__onFirstSceneLoaded.registeredGdjsCallbacks.push((runtimeScene) => {
+    gdjs.evtsExt__CrazyGamesAdApi__onFirstSceneLoaded.func(runtimeScene, runtimeScene);
+})
+gdjs.registerFirstRuntimeSceneLoadedCallback(gdjs.evtsExt__CrazyGamesAdApi__onFirstSceneLoaded.registeredGdjsCallbacks[gdjs.evtsExt__CrazyGamesAdApi__onFirstSceneLoaded.registeredGdjsCallbacks.length - 1]);
